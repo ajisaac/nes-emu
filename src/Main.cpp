274d12db56d0c404b3af3f7dd122c93cc497b115
Aaron Isaac <aaron@aisaac.co>
2022-09-30 07:34:27 -0500

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,15 @@
+#include "../headers/Console.h"
+#include <bitset>
 #include <cstddef>
 #include <iostream>
-#include <bitset>
-#include "../headers/Console.h"
-
 
 std::ostream &operator<<(std::ostream &os, std::byte b) {
-    return os << std::bitset<8>(std::to_integer<int>(b));
+  return os << std::bitset<8>(std::to_integer<int>(b));
 }
 
 int main(int argc, char *argv[]) {
-    Console console{};
-    console.run();
-    return 0;
+  Console console{};
+  console.run();
+  std::cout << "Hi " << std::endl;
+  return 0;
 }
