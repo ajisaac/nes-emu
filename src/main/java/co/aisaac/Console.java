04c995e3d39c21f4ec85f7299667849f4b299a77
Aaron Isaac <aaron@aisaac.co>
2023-12-29 00:31:14 -0800

    New Console

--- src/main/java/co/aisaac/Console.java
+++ src/main/java/co/aisaac/Console.java
@@ -1,93 +1,39 @@
 package co.aisaac;
 
 public class Console {
-	/*
+	Ram ram;
+	Cpu cpu;
+	Ppu ppu;
+	Apu apu;
 
-/// console owns all of the smaller components.
-/// you load a cartridge into a console.
-/// you can reset a cpu
+	public Console() {
+		/// create a new console
+		Ram ram = new Ram();
+		Cpu cpu = new Cpu(ram);
+		Ppu ppu = new Ppu(ram);
+		Apu apu = new Apu(ram);
 
-//    std::bitset<16> address_bus{0};  // our 16 bit bus
-//    std::bitset<8> data_bus{0};      // 8-bit data bus
-//    std::bitset<8> control_bus{0};   // 8-bit control bus
-//    Ram ram{};
-//    Cpu cpu{&ram};
+		/// set up controllers
+		/// load the cartridge
+	}
 
-pub struct Console {
-    /// cpu is the 6502 processor
-    cpu: Cpu,
-    /// ppu is the picture processing unit
-    ppu: Ppu,
-    /// apu is the audio processing unit
-    apu: Apu,
-    /// cartridge is the game cartridge
-    cartridge: Cartridge,
-    /// ram is the 2kb of ram
-    /// ram: [u8; 2048],
-    /// controller1 is the first controller
-    /// controller1: Controller,
-    /// controller2 is the second controller
-    /// controller2: Controller,
-    /// mapper is the cartridge mapper
-    /// mapper: Mapper,
-}
+	public void run() {
+		// start the console
+		while (true) {
+			step();
+		}
+	}
 
-impl Console {
-    pub(crate) fn new() -> Self {
-        /// create a new console
-        /// initialize the cpu
-        /// initialize the ppu
-        /// initialize the apu
-        /// set up controllers
-        /// load the cartridge
-        todo!()
-    }
+	public void loadCartridge(String rom) {
+		/// the .nes format should return a cartridge
+		/// this format is documented
+	}
 
-    pub(crate) fn run(&self) {
-        todo!()
-    }
+	public void reset() {
+		// reset the cpu
+	}
 
-    pub(crate) fn load_cartridge(&self, p0: &str) {
-        /// the .nes format should return a cartridge
-        /// this format is documented
-        todo!()
-    }
-
-    pub(crate) fn reset(&self) {
-        /// reset the cpu
-        todo!()
-    }
-
-    pub(crate) fn step(&self) {
-        /// step the cpu
-        /// step the ppu
-        /// step the apu
-        /// step the mapper
-        todo!()
-    }
-
-    /// what is a step frame?
-    pub(crate) fn step_frame(&self) {
-        /// step the cpu
-        /// step the ppu
-        /// step the apu
-        /// step the mapper
-        todo!()
-    }
-
-    pub(crate) fn step_seconds(&self) {
-        ///?
-        todo!()
-    }
-
-    pub(crate) fn buffer(&self) {
-        ///?
-        todo!()
-    }
-
-    pub(crate) fn save(&self) {
-        ///?
-        todo!()
-    }
-	 */
+	public void step() {
+		System.out.println("step");
+	}
 }
